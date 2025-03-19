bc71171ae0150447a54d44875b02da935dcdd575
Evan <a0966170955@gmail.com>
2025-03-19 18:00:45 +0800

    feat(ts): 利用日期型別變數紀錄產品建檔日期

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,5 @@ console.log("ProductName = ", productName);
 let isShow = true;
 console.log("isShow = ", isShow);
 
-let createDate = undefined;
+let createDate = new Date();
 console.log("Create Date = ", createDate);
